de77a0e8c5cdd9f2afd1b390125c0d6a5477b07e
LucasDiasAlves <lucas.dias7639@gmail.com>
2026-03-22 19:34:33 -0300

    FEAT - limpando a base de dados, - treinando base de dados para ter acuracia

--- site_projetos/main.py
+++ site_projetos/main.py
@@ -1,8 +1,44 @@
 import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+from sklearn.svm import LinearSVC
+from sklearn.model_selection import train_test_split
+import pandas as pd
+from sklearn.metrics import accuracy_score
 
 url = "https://gist.githubusercontent.com/guilhermesilveira/12291c548acaf544596795709020e3db/raw/325bdef098bd9cbc2189215b7e32e22f437f29f3/projetos.csv"
 
 dados = pd.read_csv(url)
 
-dados = dados["nao_finalizado"].map({1: 0, 0: 1})
-print(dados.head())
+dados["finalizados"] = dados["nao_finalizado"].map({1: 0, 0: 1})
+
+dados = dados.query("horas_esperadas > 0")
+
+# sns.relplot(x="horas_esperadas", y="preco", data=dados, hue="finalizados", col="finalizados")
+# plt.show()
+
+def treino(x, y):
+    semente = 9248
+
+    url = "https://gist.githubusercontent.com/guilhermesilveira/b9dd8e4b62b9e22ebcb9c8e89c271de4/raw/c69ec4b708fba03c445397b6a361db4345c83d7a/tracking.csv"
+    dados = pd.read_csv(url)
+
+    y = dados["comprou"]
+    x = dados[["inicial", "palestras", "contato", "patrocinio"]]
+
+    treino_x, teste_x, treino_y, teste_y = train_test_split(x,y, random_state = semente) 
+
+
+    modelo = LinearSVC()
+    modelo.fit(treino_x,treino_y)
+    previsoes = modelo.predict(teste_x)
+
+    acuracia = accuracy_score(teste_y, previsoes) * 100
+    print(f"A acuracia é de {acuracia:.2f}%")
+    print(treino_x.value_counts())
+    print(treino_y.value_counts())
+    print(teste_x.value_counts())
+
+treino(dados[["horas_esperadas", "preco"]], dados["finalizados"])
+
+# print(dados.head())
---
+++ site_projetos/readme.md
@@ -0,0 +1,12 @@
+## Site de projetos
+
+descobrindo em uma base de dados de um site de projetos, qunatos projetos foram finalziados, baseando em :
+- horas de projeto;
+- preço do projeto;
+
+Plotamos 2 gráficos:
+- finalizados;
+- não finalizados;
+Para assim podermos entender como a relação de preço pelas horas trabalham.
+
+Limpamos a base de dados para ter apenas horas_esperadas > 0; pois ocorria algum erro na base de dados com esses horarios
