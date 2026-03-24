97926fa0edaf80278eb37d22d09f14724f991f88
LucasDiasAlves <lucas.dias7639@gmail.com>
2026-03-24 08:57:09 -0300

    REFACTOR/DEBUG - Arrumando função treino, para recceber os valores do nosso novo dataset

--- site_projetos/main.py
+++ site_projetos/main.py
@@ -20,24 +20,18 @@ dados = dados.query("horas_esperadas > 0")
 def treino(x, y):
     semente = 9248
 
-    url = "https://gist.githubusercontent.com/guilhermesilveira/b9dd8e4b62b9e22ebcb9c8e89c271de4/raw/c69ec4b708fba03c445397b6a361db4345c83d7a/tracking.csv"
-    dados = pd.read_csv(url)
-
-    y = dados["comprou"]
-    x = dados[["inicial", "palestras", "contato", "patrocinio"]]
-
     treino_x, teste_x, treino_y, teste_y = train_test_split(x,y, random_state = semente) 
-
 
     modelo = LinearSVC()
     modelo.fit(treino_x,treino_y)
     previsoes = modelo.predict(teste_x)
 
     acuracia = accuracy_score(teste_y, previsoes) * 100
-    print(f"A acuracia é de {acuracia:.2f}%")
-    print(treino_x.value_counts())
+    print(f"A acurácia é de {acuracia:.2f}%\n")    
+    print("Distribuição das classes no treino:")
     print(treino_y.value_counts())
-    print(teste_x.value_counts())
+    print("\nDistribuição das classes no teste:")
+    print(teste_y.value_counts())
 
 treino(dados[["horas_esperadas", "preco"]], dados["finalizados"])
 
