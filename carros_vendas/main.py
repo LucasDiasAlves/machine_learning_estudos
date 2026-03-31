c49854052f1d65f1dfeddddb3dd3e1b8c9d1ccb2
LucasDiasAlves <lucas.dias7639@gmail.com>
2026-03-31 10:56:02 -0300

    FEAT, - alterando o nome da variavel para utilziar outro metodo de machine learning, o Dummy classifier

--- carros_vendas/main.py
+++ carros_vendas/main.py
@@ -9,6 +9,7 @@ import pandas as pd
 from sklearn.metrics import accuracy_score
 import numpy as np
 from sklearn.preprocessing import StandardScaler
+from sklearn.dummy import DummyClassifier
 
 # --------------------------------------------------------------------------------------------
 # -------------------------------------- TRATANDO OS DADOS------------------------------------
@@ -20,6 +21,9 @@ dados["idade"] = datetime.today().year - dados["ano_do_modelo"]
 dados.drop(["milhas_por_ano","ano_do_modelo"], axis=1 , inplace=True)
 # ------------------------------------------------------------------------------------------------------
 
+
+
+
 semente = 5719
 
 x = dados[['preco','idade','km_por_ano']]
@@ -30,12 +34,16 @@ raw_treino_x, raw_teste_x, treino_y, teste_y = train_test_split(x,y, random_stat
 scaler = StandardScaler()
 scaler.fit(raw_treino_x)
 
-treino_x = scaler.transform(raw_treino_x)
-teste_x = scaler.transform(raw_teste_x)
+# treino_x = scaler.transform(raw_treino_x)
+# teste_x = scaler.transform(raw_teste_x)
 
-modelo = SVC(gamma='auto')
-modelo.fit(treino_x,treino_y)
-previsoes = modelo.predict(teste_x)
+classificador = DummyClassifier()
+classificador.fit(raw_treino_x, treino_y)
+previsoes = classificador.predict(raw_teste_x)
+
+# modelo = SVC(gamma='auto')
+# modelo.fit(treino_x,treino_y)
+# previsoes = modelo.predict(teste_x)
 acuracia = accuracy_score(teste_y, previsoes) * 100
 
 print(f"A acurácia é de {acuracia:.2f}%\n")    
@@ -45,7 +53,7 @@ print("\nDistribuição das classes no teste:")
 print(teste_y.value_counts())
 
 
-sns.relplot(x="km_por_ano'", y="preco", data=dados, hue="vendido", col="vendido")
+sns.relplot(x="km_por_ano", y="preco", data=dados, hue="vendido", col="vendido")
 plt.show()
 
 print(dados.columns)
