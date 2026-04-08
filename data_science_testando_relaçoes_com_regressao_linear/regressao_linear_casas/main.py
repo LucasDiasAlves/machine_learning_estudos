2e214fe1852b325ac557a03696e318fdd5b6e507
LucasDiasAlves <lucas.dias7639@gmail.com>
2026-04-08 16:32:03 -0300

    FEAT, - ajustado outros modelos e comparando os

--- data_science_testando_relaçoes_com_regressao_linear/regressao_linear_casas/main.py
+++ data_science_testando_relaçoes_com_regressao_linear/regressao_linear_casas/main.py
@@ -2,6 +2,7 @@ import pandas as pd
 from sklearn.model_selection import train_test_split 
 from statsmodels.formula.api import ols
 from sklearn.metrics import r2_score
+import statsmodels.api as sm
 import matplotlib as plt
 
 base_dados = './data_science_testando_relaçoes_com_regressao_linear/regressao_linear_casas/Preços_de_casas.csv'
@@ -21,18 +22,45 @@ df_train['preco_de_venda'] = y_train
 
 modelo_0 = ols('preco_de_venda ~ area_primeiro_andar', data = df_train).fit()
 
+
+x_train = sm.add_constant()
+print(x_train.head)
+
+modelo_1 = sm.OLS(y_train, x_train[['const','existe_segundo_andar','area_segundo_andar','quantidade_banheiros','capacidade_carros_garagem','qualidade_da_cozinha_Excelente']]).fit
+
+modelo_2 = sm.OLS(y_train,x_train[['const','existe_segundo_andar','quantidade_banheiros','capacidade_carros_garagem','qualidade_da_cozinha_Excelente']]).fit
+
+modelo_3 = sm.OLS(y_train, x_train[['const','existe_segundo_andar','quantidade_banheiros','qualidade_da_cozinha_Excelente']]).fit
+
 print(modelo_0.params)
+# print(modelo_1.params)
+# print(modelo_2.params)
+# print(modelo_3.params)
+
 print(modelo_0.summary())
-print(modelo_0.rsquared)
+print(modelo_1.summary())
+print(modelo_2.summary())
+print(modelo_3.summary())
+
+
 
 print(modelo_0.resid)
-print(modelo_0.resid.hist())
-plt.tittle("Distribuiçãp dos residuos")
-plt.show()
 
+
+print(modelo_0.resid.hist())
+
+# plt.tittle("Distribuiçãp dos residuos")
+# plt.show()
 
 y_predict = modelo_0.predict(x_test)
+print("R²")
+print("Modelo 0: ", modelo_0.rsquared)
+print("Modelo 1: ", modelo_1.rsquared)
+print("Modelo 2: ", modelo_2.rsquared)
+print("Modelo 3: ", modelo_3.rsquared)
+
 print("R²: ", r2_score(y_test, y_predict))
+
 
 print(corr['preco_de_venda'])
 print(dados.columns)
