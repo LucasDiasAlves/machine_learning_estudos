b4a1f763ce1bc51124050cb3105b6a3edce6de92
LucasDiasAlves <lucas.dias7639@gmail.com>
2026-04-14 17:12:53 +0000

    FEAT/BUILD /DEBUG - adicionando bibliotecca ao requeriments.txt; - adicionando desafio da alura

--- data_science_testando_relaçoes_com_regressao_linear/regressao_linear_casas/main.py
+++ data_science_testando_relaçoes_com_regressao_linear/regressao_linear_casas/main.py
@@ -9,44 +9,45 @@ base_dados = './data_science_testando_relaçoes_com_regressao_linear/regressao_l
 dados = pd.read_csv(base_dados)
 dados = dados.drop(columns='Id') #retirando a coluna ID pois não é eficiente para o treinamento.
 corr = dados.corr()
+
 y = dados['preco_de_venda']
 x = dados.drop(columns='preco_de_venda')
 
 
 x_train, x_test, y_train, y_test = train_test_split(x,y , test_size = 0.4 , random_state = 230)
+
 df_train = pd.DataFrame(data = x_train)
 df_train['preco_de_venda'] = y_train
-
-modelo_0 = ols('preco_de_venda ~ area_primeiro_andar', data = df_train).fit()
-
 
 x_train = sm.add_constant(x_train)
 # print(x_train.head)
 
+modelo_0 = ols('preco_de_venda ~ area_primeiro_andar', data = df_train).fit()
+
 modelo_1 = sm.OLS(y_train, x_train[['const','existe_segundo_andar','area_segundo_andar','quantidade_banheiros','capacidade_carros_garagem','qualidade_da_cozinha_Excelente']]).fit()
-
 modelo_2 = sm.OLS(y_train,x_train[['const','existe_segundo_andar','quantidade_banheiros','capacidade_carros_garagem','qualidade_da_cozinha_Excelente']]).fit()
+modelo_3 = sm.OLS(y_train, x_train[['const','area_primeiro_andar','existe_segundo_andar','quantidade_banheiros','qualidade_da_cozinha_Excelente']]).fit()
 
-modelo_3 = sm.OLS(y_train, x_train[['const','existe_segundo_andar','quantidade_banheiros','qualidade_da_cozinha_Excelente']]).fit()
-
-# print(modelo_0.summary())
+# print(modelo_2.summary())
 
 # print(modelo_0.resid)
 # print(modelo_0.resid.hist())
 # plt.tittle("Distribuiçãp dos residuos")
 # plt.show()
 
-y_predict = modelo_0.predict(x_test)
-print("R²")
-print("Modelo 0: ", modelo_0.rsquared)
-print("Modelo 1: ", modelo_1.rsquared)
-print("Modelo 2: ", modelo_2.rsquared)
-print("Modelo 3: ", modelo_3.rsquared)
+# y_predict = modelo_0.predict(x_test)
 
-print(len(modelo_0.params)) # retorna o numero de parametros do modelo
-print(len(modelo_1.params))
-print(len(modelo_2.params))
+# print("R²")
+# print("Modelo 0: ", modelo_0.rsquared) # Para verificar o valor de R² do modelo, quanto mais próximo de 1 melhor o modelo se encaixa aos dados.
+# print("Modelo 1: ", modelo_1.rsquared)
+# print("Modelo 2: ", modelo_2.rsquared)
+# print("Modelo 3: ", modelo_3.rsquared)
+
+# print(len(modelo_0.params)) # retorna o numero de parametros do modelo
+# print(len(modelo_1.params))
+# print(len(modelo_2.params))
 print(len(modelo_3.params))
+print(modelo_3.params)
 
 # print(corr['preco_de_venda'])
 # print(dados.columns)
